b2eca16ab44981f0a9c4417447af1b0171682ef9
lopezbaeza <javier.lopez.baeza.churruca@gmail.com>
2022-07-28 11:24:30 +0200

    Explicaciones y configuraciones

---
+++ basico/Ejercicios/settings.py
@@ -0,0 +1,10 @@
+import os
+
+# C://Users/nombre usuario/...
+# /home/nombre usuario/...
+
+# script_dir = os.path.dirname( __file__ )
+# mymodule_dir = os.path.join( script_dir, '....', 'media')
+
+#MEDIA_ROOT = os.path.expanduser(mymodule_dir)
+MEDIA_ROOT = os.path.expanduser("~Matusaleno\\Documents\\Apuntes\\Curso Data Science\\Ejercicios\\Míos\\Dataset\\git_test\\Data-Science\\media\\")
